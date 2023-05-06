c92dad97bff5d152372110f0aadc2029bcc44bd3
Yoriki Yamaguchi <yorikiyamaguchi@gmail.com>
2023-05-06 20:19:46 +0900

    v0.1.0

--- core/src/index.test.ts
+++ core/src/index.test.ts
@@ -1,4 +1,5 @@
 import "jest";
+import { dump, load } from "js-yaml";
 
 import { sortKeys } from ".";
 
@@ -8,18 +9,198 @@ test("make sure Object.keys() can be used to test", () => {
   expect(["a", "b"]).not.toEqual(["b", "a"]);
 });
 
-describe("sortKeys", () => {
+describe("sortKeys()", () => {
+  const original = {
+    b: 1,
+    d: 2,
+    c: [
+      { m: 11, n: 12 },
+      { e: 3, f: 4, d: 5 },
+    ],
+    a: {
+      g: 6,
+      i: 7,
+      h: 8,
+      j: {
+        l: 9,
+        k: 10,
+      },
+    },
+  };
+
   it("should sort keys in ascending order by default", () => {
-    expect(Object.keys(sortKeys({ b: 1, a: 2 }))).toEqual(
-      Object.keys({ a: 2, b: 1 })
-    );
+    expect(Object.keys(sortKeys(original))).toEqual(["a", "b", "c", "d"]);
   });
 
   it("should not mutate the given object", () => {
-    const object = { b: 1, a: 2 };
-    const objectJson = JSON.stringify(object);
-    const result = sortKeys(object);
-    expect(object).not.toBe(result);
-    expect(JSON.stringify(object)).toEqual(objectJson);
+    const originalJson = JSON.stringify(original);
+    sortKeys(original);
+    expect(JSON.stringify(original)).toEqual(originalJson);
   });
+
+  it("should return a new object", () => {
+    expect(sortKeys(original)).not.toBe(original);
+  });
+
+  it("should maintain the values of the given object", () => {
+    expect(sortKeys(original)).toEqual(original);
+  });
+
+  it("should sort keys in nested objects recursively by default", () => {
+    const result = sortKeys(original);
+    expect(Object.keys(result.a.j)).toEqual(["k", "l"]);
+  });
+
+  it("should sort keys in objects in an array recursively by default", () => {
+    const result = sortKeys(original);
+    expect(Object.keys(result.c[1])).toEqual(["d", "e", "f"]);
+  });
+
+  describe("option", () => {
+    describe("option.depth", () => {
+      it("should sort keys in nested objects up to the given depth", () => {
+        const result = sortKeys(original, { depth: 1 });
+        expect(Object.keys(result.a.j)).toEqual(["l", "k"]);
+      });
+
+      it("should sort keys in objects in an array up to the given depth", () => {
+        const result = sortKeys(original, { depth: 1 });
+        expect(Object.keys(result.c[1])).toEqual(["e", "f", "d"]);
+      });
+    });
+
+    describe("option.compare", () => {
+      it("should accept a compare function generator", () => {
+        const result = sortKeys(original, {
+          compare: () => () => 0,
+        });
+
+        expect(Object.keys(result)).toEqual(["b", "d", "c", "a"]);
+
+        const result2 = sortKeys(original, {
+          compare: (object) => (left, right) => {
+            // sort by key name, but if the value is 1, put it at the end
+            if (object[left] === 1) return 1;
+            if (object[right] === 1) return -1;
+            return left.localeCompare(right);
+          },
+        });
+
+        expect(Object.keys(result2)).toEqual(["a", "c", "d", "b"]);
+      });
+
+      it("should ignore prioritize option if a custom compare function generator is given", () => {
+        const result = sortKeys(original, {
+          compare: () => () => 0,
+          prioritize: {
+            keys: ["d", "c"],
+          },
+        });
+
+        expect(Object.keys(result)).toEqual(["b", "d", "c", "a"]);
+      });
+    });
+
+    describe("option.prioritize", () => {
+      describe("option.prioritize.keys", () => {
+        it("should prioritize keys in the given order", () => {
+          const result = sortKeys(original, {
+            prioritize: {
+              keys: ["d", "c"],
+            },
+          });
+
+          expect(Object.keys(result)).toEqual(["d", "c", "a", "b"]);
+        });
+      });
+
+      describe("option.prioritize.primitives", () => {
+        it("should prioritize primitives", () => {
+          const result = sortKeys(original, {
+            prioritize: {
+              primitives: true,
+            },
+          });
+
+          expect(Object.keys(result)).toEqual(["b", "d", "a", "c"]);
+        });
+
+        it("should prioritize prioritize.keys even if it's a non-premitive", () => {
+          const result = sortKeys(original, {
+            prioritize: {
+              keys: ["c"],
+              primitives: true,
+            },
+          });
+
+          expect(Object.keys(result)).toEqual(["c", "b", "d", "a"]);
+        });
+      });
+    });
+  });
+});
+
+test("Kubernetes manifest example", () => {
+  const input = `apiVersion: v1
+data:
+  ca.crt: |
+    -----BEGIN CERTIFICATE-----
+    snip
+    -----END CERTIFICATE-------
+kind: ConfigMap
+metadata:
+  annotations:
+    kubernetes.io/description: Contains a CA bundle that can be used to verify the
+      kube-apiserver when using internal endpoints such as the internal service IP
+      or kubernetes.default.svc. No other usage is guaranteed across distributions
+      of Kubernetes clusters.
+  creationTimestamp: "2023-01-11T06:17:08Z"
+  name: kube-root-ca.crt
+  namespace: default
+  resourceVersion: "254"
+  uid: 5e337512-5239-4e08-88ed-e6003a81786b
+`;
+
+  const output = `apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: kube-root-ca.crt
+  namespace: default
+  annotations:
+    kubernetes.io/description: >-
+      Contains a CA bundle that can be used to verify the kube-apiserver when
+      using internal endpoints such as the internal service IP or
+      kubernetes.default.svc. No other usage is guaranteed across distributions
+      of Kubernetes clusters.
+  creationTimestamp: '2023-01-11T06:17:08Z'
+  resourceVersion: '254'
+  uid: 5e337512-5239-4e08-88ed-e6003a81786b
+data:
+  ca.crt: |
+    -----BEGIN CERTIFICATE-----
+    snip
+    -----END CERTIFICATE-------
+`;
+
+  const result = dump(
+    sortKeys(load(input) as Record<string, unknown>, {
+      prioritize: {
+        primitives: true,
+        keys: [
+          "apiVersion",
+          "kind",
+          "metadata",
+          "name",
+          "namespace",
+          "labels",
+          "annotations",
+          "spec",
+          "data",
+          "stringData",
+        ],
+      },
+    })
+  );
+
+  expect(result).toEqual(output);
 });
--- core/src/index.ts
+++ core/src/index.ts
@@ -1,31 +1,92 @@
-type Option<T> = {
-  recurse?: boolean;
+type CompareFunctionGenerator = (
+  object: Record<string, unknown>
+) => (leftKey: string, rightKey: string) => number;
 
-  compare?: (
-    leftKey: keyof T,
-    rightKey: keyof T,
-    object?: Record<keyof T, unknown>
-  ) => number;
+type Option = {
+  depth?: number;
+  compare?: CompareFunctionGenerator;
 
   prioritize?: {
-    keys?: (keyof T)[];
+    keys?: string[];
     primitives?: boolean;
   };
 };
 
-export function sortKeys<T extends object>(obj: T, option: Option<T> = {}): T {
-  const keys = Object.keys(obj) as Array<keyof T>;
+function isPremitive(value: unknown): boolean {
+  return (
+    typeof value === "string" ||
+    typeof value === "number" ||
+    typeof value === "boolean" ||
+    typeof value === "undefined" ||
+    value === null
+  );
+}
 
-  const compareFunction =
+export function sortKeys<T extends Record<string, unknown>>(
+  object: T,
+  option: Option = {}
+): T {
+  const compare: CompareFunctionGenerator =
     option.compare ||
-    ((leftKey, rightKey) =>
-      (leftKey as string).localeCompare(rightKey as string));
+    ((object2) => (leftKey, rightKey) => {
+      if (option.prioritize?.keys) {
+        const leftKeyIndex = option.prioritize.keys.indexOf(leftKey);
+        const rightKeyIndex = option.prioritize.keys.indexOf(rightKey);
 
-  keys.sort(compareFunction);
+        if (leftKeyIndex !== -1) {
+          if (rightKeyIndex !== -1) {
+            return leftKeyIndex - rightKeyIndex;
+          } else {
+            return -1;
+          }
+        } else if (rightKeyIndex !== -1) {
+          return 1;
+        }
+      }
 
-  return keys.reduce((acc, key) => {
-    const value = obj[key];
+      if (option.prioritize?.primitives) {
+        const leftIsPrimitive = isPremitive(object2[leftKey]);
+        const rightIsPrimitive = isPremitive(object2[rightKey]);
 
-    return { ...acc, [key]: value };
-  }, {} as T);
+        if (leftIsPrimitive) {
+          if (!rightIsPrimitive) {
+            return -1;
+          }
+        } else if (rightIsPrimitive) {
+          return 1;
+        }
+      }
+
+      return leftKey.localeCompare(rightKey);
+    });
+
+  function recurse(
+    value: unknown,
+    option2: Option & { depth: number }
+  ): unknown {
+    if (option2.depth === 0 || isPremitive(value)) return value;
+
+    const nextRecursionOption = { ...option2, depth: option2.depth - 1 };
+
+    if (Array.isArray(value)) {
+      return value.map((element) => recurse(element, nextRecursionOption));
+    }
+
+    const object2 = value as Record<string, unknown>;
+
+    const keys = Object.keys(object2);
+
+    keys.sort(compare(object2));
+
+    return keys.reduce((acc, key) => {
+      const value2 = object2[key];
+
+      return {
+        ...acc,
+        [key]: sortKeys(value2 as Record<string, unknown>, nextRecursionOption),
+      };
+    }, {} as T);
+  }
+
+  return recurse(object, { ...option, depth: option.depth ?? Infinity }) as T;
 }
