8f18fcc7683bc0e77ec6c0c04ac4fc43c6fb0016
Yoriki Yamaguchi <yorikiyamaguchi@gmail.com>
2024-10-19 20:58:01 +0900

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -20,64 +20,67 @@ function isPrimitive(value: unknown): boolean {
   );
 }
 
-export function sortKeys<T extends Record<string, unknown>>(object: T, option: Option = {}): T {
-  const compare: CompareFunctionGenerator =
-    option.compare ??
-    ((object2) => (leftKey, rightKey) => {
-      if (option.prioritize?.keys) {
-        const leftKeyIndex = option.prioritize.keys.indexOf(leftKey);
-        const rightKeyIndex = option.prioritize.keys.indexOf(rightKey);
+function recurse(value: unknown, option: Option & { depth: number; compare: CompareFunctionGenerator }): unknown {
+  if (option.depth === 0 || isPrimitive(value)) return value;
 
-        if (leftKeyIndex !== -1) {
-          if (rightKeyIndex !== -1) {
-            return leftKeyIndex - rightKeyIndex;
-          }
+  const nextRecursionOption = { ...option, depth: option.depth - 1 };
 
-          return -1;
-        }
-
-        if (rightKeyIndex !== -1) {
-          return 1;
-        }
-      }
-
-      if (option.prioritize?.primitives) {
-        const leftIsPrimitive = isPrimitive(object2[leftKey]);
-        const rightIsPrimitive = isPrimitive(object2[rightKey]);
-
-        if (leftIsPrimitive) {
-          if (!rightIsPrimitive) {
-            return -1;
-          }
-        } else if (rightIsPrimitive) {
-          return 1;
-        }
-      }
-
-      return leftKey.localeCompare(rightKey);
-    });
-
-  function recurse(value: unknown, option2: Option & { depth: number }): unknown {
-    if (option2.depth === 0 || isPrimitive(value)) return value;
-
-    const nextRecursionOption = { ...option2, depth: option2.depth - 1 };
-
-    if (Array.isArray(value)) {
-      return value.map((element) => recurse(element, nextRecursionOption));
-    }
-
-    const object2 = value as Record<string, unknown>;
-
-    const keys = Object.keys(object2);
-
-    keys.sort(compare(object2));
-
-    return Object.fromEntries(
-      keys.map((key) => {
-        return [key, recurse(object2[key], nextRecursionOption)];
-      }),
-    );
+  if (Array.isArray(value)) {
+    return value.map((element) => recurse(element, nextRecursionOption));
   }
 
-  return recurse(object, { ...option, depth: option.depth ?? Number.POSITIVE_INFINITY }) as T;
+  const object = value as Record<string, unknown>;
+
+  // TODO: Use .toSorted() after dropping support for Node 18
+  // const keys = Object.keys(object).toSorted(option.compare(object));
+  const keys = Object.keys(object);
+  keys.sort(option.compare(object));
+
+  return Object.fromEntries(
+    keys.map((key) => {
+      return [key, recurse(object[key], nextRecursionOption)];
+    }),
+  );
+}
+
+function getDefaultCompare(option: Option): CompareFunctionGenerator {
+  return (object2) => (leftKey, rightKey) => {
+    if (option.prioritize?.keys) {
+      const leftKeyIndex = option.prioritize.keys.indexOf(leftKey);
+      const rightKeyIndex = option.prioritize.keys.indexOf(rightKey);
+
+      if (leftKeyIndex !== -1) {
+        if (rightKeyIndex !== -1) {
+          return leftKeyIndex - rightKeyIndex;
+        }
+
+        return -1;
+      }
+
+      if (rightKeyIndex !== -1) {
+        return 1;
+      }
+    }
+
+    if (option.prioritize?.primitives) {
+      const leftIsPrimitive = isPrimitive(object2[leftKey]);
+      const rightIsPrimitive = isPrimitive(object2[rightKey]);
+
+      if (leftIsPrimitive) {
+        if (!rightIsPrimitive) {
+          return -1;
+        }
+      } else if (rightIsPrimitive) {
+        return 1;
+      }
+    }
+
+    return leftKey.localeCompare(rightKey);
+  };
+}
+
+export function sortKeys<T extends Record<string, unknown>>(object: T, option: Option = {}): T {
+  const compare = option.compare ?? getDefaultCompare(option);
+
+  return recurse(object, { ...option, compare, depth: option.depth ?? Number.POSITIVE_INFINITY }) as T;
 }
